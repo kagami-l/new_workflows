79b3b46235605eb8e5d256d2501b10c9a9dcff04
kagami <mingjing@merico.dev>
2024-04-12 13:24:06 +0800

    Add examples of sharing envs in nested workflows

---
+++ merico/workflows/env_mine/c2/main_c2.py
@@ -0,0 +1,26 @@
+
+import sys
+
+def main():
+    print(f"\n\n## Hello from env_mine.c2! Should use my-py-1 \n\n", flush=True)
+
+    print(f"\n\npython version: {sys.version}\n\n")
+    for p in sys.path:
+        print(f"- {p}")
+    print("\n\n", flush=True)
+
+    print("\n\n### Checking additional packages\n\n")
+    try:
+        import typer
+        print(f"\n\n- typer version: {typer.__version__}")
+    except ImportError:
+        print("\n\n- typer not found")
+
+    try:
+        import numpy
+        print(f"\n\n- numpy version: {numpy.__version__}")
+    except ImportError:
+        print("\n\n- numpy not found")
+
+if __name__ == "__main__":
+    main()
